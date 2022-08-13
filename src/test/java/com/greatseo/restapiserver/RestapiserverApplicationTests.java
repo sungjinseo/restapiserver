21d4e275654cfaabf61497b9fd0468af4a71417b
sjseo85 <sjseo85@gmail.com>
2022-08-14 01:04:58 +0900

    first commit

---
+++ src/test/java/com/greatseo/restapiserver/RestapiserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greatseo.restapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
